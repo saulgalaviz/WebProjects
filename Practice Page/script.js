a1f99273bf0484d0d35d2e1725f8ee6fb214f7b6
Nostrama <nostramagear@gmail.com>
2018-06-26 12:44:33 -0700

    Uploaded Source Code

---
+++ Practice Page/script.js
@@ -0,0 +1,12 @@
+function changeColor(property)
+{
+    text = document.getElementById(property);
+
+    text.style["color"] = "#" + Math.floor(Math.random() * 1000000);
+}
+
+function displayTwoLinks(property)
+{
+    if(Math.floor(Math.random() * 2) == 1);
+
+}
